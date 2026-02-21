7170a241560bc5d9e19c40a9c4ac37f4104522d7
MKEITA22 <kmohamedlamine1@gmail.com>
2026-02-21 22:47:43 +0000

    mise à jour version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.mec.citoyenplus"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
     }
 
